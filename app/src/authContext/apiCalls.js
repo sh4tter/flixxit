d649280fa95e3a5d75b62665e1c7c0fa070493f0
blueyottle <sunnydowari@gmail.com>
2023-07-18 13:17:43 +0530

    bug fixes in context

--- app/src/authContext/apiCalls.js
+++ app/src/authContext/apiCalls.js
@@ -1,14 +1,10 @@
 import axios from "axios";
 import { loginFailure, loginStart, loginSuccess } from "./AuthActions";
 
-export const login = async (user, dispatch, isAdmin = false) => {
+export const login = async (user, dispatch) => {
   dispatch(loginStart());
   try {
-    let url = "auth/login";
-    if (isAdmin) {
-      url = "auth/admin";
-    }
-    const res = await axios.post(url, user);
+    const res = await axios.post("auth/login", user);
     dispatch(loginSuccess(res.data));
   } catch (err) {
     dispatch(loginFailure());
