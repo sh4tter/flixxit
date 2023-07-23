bf7ee0831aa4f7a69e4c94476ebd98f11b9d133c
blueyottle <sunnydowari@gmail.com>
2023-07-23 23:34:52 +0530

    server bugs

--- server/index.js
+++ server/index.js
@@ -8,12 +8,22 @@ const path = require("path");
 
 //ignoring env files in git
 dotenv.config();
+// Serve the React app's index.html for the root route
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+//connect to mongodb using mongoose using .env file
+
+app.use(express.json());
+
 app.use(
   cors({
-    origin: "*",
+    origin: "https://flixxit-backend-blueyottle.vercel.app",
     credentials: true,
   })
 );
+app.options("*", cors());
+
 app.use(express.static("public"));
 app.use(
   express.urlencoded({
@@ -21,11 +31,9 @@ app.use(
   })
 );
 
-// Serve the React app's index.html for the root route
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-});
-//connect to mongodb using mongoose using .env file
+//use the auth router, to get the user
+app.use("/api", router);
+
 mongoose
   .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
@@ -36,11 +44,6 @@ mongoose
   .catch((err) => {
     console.error(err);
   });
-
-app.use(express.json());
-
-//use the auth router, to get the user
-app.use("/api", router);
 
 app.listen(8800, () => {
   console.log(`server is running`);
