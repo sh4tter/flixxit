e2e024a3d41f9d122e2177b09a52db0f9452e04e
blueyottle <sunnydowari@gmail.com>
2023-07-30 04:26:24 +0530

    main_page_logo

--- app/src/components/navbar/Navbar.jsx
+++ app/src/components/navbar/Navbar.jsx
@@ -18,7 +18,7 @@ const Navbar = () => {
       <div className="container">
         <div className="left">
           <img
-            src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png"
+            src="https://imgtr.ee/images/2023/07/29/874aa6d159c78664eba369521a387358.webp"
             alt=""
           />
           <Link to="/" className="link">
@@ -30,13 +30,13 @@ const Navbar = () => {
           <Link to="/movies" className="link">
             <span className="navbarmainLinks">Movies</span>
           </Link>
-          <span>New and Popular</span>
-          <span>My List</span>
+          {/* <span>New and Popular</span>
+          <span>My List</span> */}
         </div>
         <div className="right">
-          <Search className="icon" />
+          {/* <Search className="icon" />
           <span>KID</span>
-          <Notifications className="icon" />
+          <Notifications className="icon" /> */}
           <img
             src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
             alt=""
